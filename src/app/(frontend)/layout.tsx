5ed3ca6d428f8d84ac3371ed53667314dfa3a682
Yoana <ylfranco98@gmail.com>
2025-04-13 11:12:56 -0400

    first commit

---
+++ src/app/(frontend)/layout.tsx
@@ -0,0 +1,15 @@
+import "@/app/globals.css";
+import { SanityLive } from "@/sanity/lib/live";
+
+export default function FrontendLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      {children}
+      <SanityLive />
+    </>
+  );
+}
