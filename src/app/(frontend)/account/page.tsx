35863481c44ef0f1bc358e0d58e67567828d9893
Yoana <ylfranco98@gmail.com>
2025-05-18 22:46:21 -0400

    add profile page

---
+++ src/app/(frontend)/account/page.tsx
@@ -0,0 +1,9 @@
+import { Title } from "@/components/Title";
+
+export default async function Page() {
+  return (
+    <section className="mt-[250px] mx-[10%] p-0 ">
+      <Title>Profile</Title>
+    </section>
+  );
+}
